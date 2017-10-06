259b34cda4dfe25ff068e1afcd981a918c8b1f04
student <mwoolery@ecusers-imac-90.nwmissouri.edu>
2017-10-06 13:57:53 -0500

    added the project to the repo

---
+++ GymMachineAssistant/GymMachineAssistant/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GymMachineAssistant
+//
+//  Created by Woolery,Matthew A on 10/6/17.
+//  Copyright © 2017 Woolery,Matthew A. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
