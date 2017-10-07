a524b28fd7b1587b55ade6779718a29d71e64c30
Student <student@nw44725.nwmissouri.edu>
2017-10-06 22:34:02 -0500

    started to work on adding stub data for the tableview.  Will continue working on it at another time

--- GymMachineAssistant/GymMachineAssistant/listofallmachines.swift
+++ GymMachineAssistant/GymMachineAssistant/listofallmachines.swift
@@ -8,20 +8,56 @@
 
 import UIKit
 
-class listofallmachines : UIViewController {
+class MyTableViewCell : UITableViewCell {
     
-    let xyz = "Test"
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+}
 
+class listofallmachines: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    let machines = ["bench", "squat", "fly", "dips"]
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //gets count for sections
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return machines.count
+    }
+    // makes the cells
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        //title
+        cell.textLabel?.text = machines[indexPath.row]
+        //subtitle will add info to later
+        cell.detailTextLabel?.text = ""
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
     }
-
+    // refreshes the data each visit
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
