ef469327e20f7c4d145d286722a8e818b2a4209b
student <student@Amarendars-MacBook-Air.local>
2017-11-02 22:11:04 -0500

    Added home page

---
+++ GymMachineAssistant/GymMachineAssistant/HomeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomeViewController.swift
+//  GymMachineAssistant
+//
+//  Created by student on 11/2/17.
+//  Copyright © 2017 Woolery,Matthew A. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let when = DispatchTime.now() + 4
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            self.performSegue(withIdentifier: "navigateToTabs", sender: self)
+        }
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GymMachineAssistant/GymMachineAssistant/MapViewController.swift
+++ GymMachineAssistant/GymMachineAssistant/MapViewController.swift
@@ -13,13 +13,17 @@ class MapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBAction func goToPin(_ sender: Any) {
+        
+        self.performSegue(withIdentifier: "gymViewSegue", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         let distance:CLLocationDegrees = 2000
         let gymMapLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(40.3505584, -94.88488749999999)
         mapView.setRegion(MKCoordinateRegionMakeWithDistance(gymMapLocation, distance, distance), animated: true)
         
-        let gymPinLoc = GymMapLoc(title: "Student Recreation Center", subtitle: "Foster's Fitness Gym", coordinate: gymMapLocation )
+        let gymPinLoc = GymMapLoc(title: "Student Recreation Center", subtitle: "Foster's Fitness Gym", coordinate: gymMapLocation)
         mapView.addAnnotation(gymPinLoc)
         
         // Do any additional setup after loading the view.
